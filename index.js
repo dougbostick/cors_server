12d85d575731e68f084cce14f41e5f435d551b0c
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-01-11 17:17:28 -0500

    removed console log of key

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 const cors = require('cors');
 const axios = require('axios');
 
-console.log(process.env.REACT_APP_API_KEY);
-
 app.use(
   cors({
     origin: 'https://dougbostick.github.io',
@@ -21,7 +19,6 @@ app.get('/getPlaces', async (req, res, next) => {
   try {
     const lat = req.query.lat;
     const lng = req.query.lng;
-    const key = req.query.key;
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=restaurant&key=${process.env.REACT_APP_API_KEY}`
     );
