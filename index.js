5c415bffb63305b016d12e30d05bf941bbe6e7ec
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-01-11 15:51:30 -0500

    removed !

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const axios = require('axios');
 
+console.log(process.env);
+
+
 app.use(
   cors({
     origin: 'https://dougbostick.github.io',
@@ -12,14 +15,14 @@ app.use(
 app.use(express.json());
 
 app.get('/message', (req, res, next) => {
-  res.send('message from express server!!');
+  res.send('message from express server');
 });
 
 app.post('/getPlaces', async (req, res, next) => {
   try {
     const lat = req.body.lat;
     const lng = req.body.lng;
-    const key = req.body.key;
+    const key = process.env.REACT_APP_API_KEY;
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=restaurant&key=${key}`
     );
