29281e51e3c5a43f3a02dc272a98b031d6d9cf81
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-01-11 17:05:05 -0500

    replaced key with .env

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const axios = require('axios');
 
 console.log(process.env.REACT_APP_API_KEY);
 
-
 app.use(
   cors({
     origin: 'https://dougbostick.github.io',
@@ -20,12 +19,11 @@ app.get('/message', (req, res, next) => {
 
 app.get('/getPlaces', async (req, res, next) => {
   try {
-    console.log('PARAMS', req.params)
     const lat = req.query.lat;
     const lng = req.query.lng;
     const key = req.query.key;
     const response = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=restaurant&key=${key}`
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=restaurant&key=${process.env.REACT_APP_API_KEY}`
     );
     res.send(response.data);
   } catch (err) {
