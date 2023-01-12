21910a91bfbe18dc13d9938d278ee9e4e06bdafb
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-01-12 15:27:11 -0500

    trying one key

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const axios = require('axios');
 
 
-console.log('KEY', process.env.REACT_APP_API_KEY)
 app.use(
   cors({
     origin: 'https://dougbostick.github.io',
